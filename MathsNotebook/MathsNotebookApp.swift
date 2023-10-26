eb17608b973b0657842ee0a89712c3fc60237656
sah-sah <67882120+sah-sah@users.noreply.github.com>
2023-10-26 16:42:36 +1100

    Initial Commit

---
+++ MathsNotebook/MathsNotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathsNotebookApp.swift
+//  MathsNotebook
+//
+//  Created by Stephen Howe on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MathsNotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
